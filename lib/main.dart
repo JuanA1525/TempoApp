c9c9ae42ca0a63dae66528bb4f14b54b99e750cb
Juan Andres Porras <juana.porras@upb.edu.co>
2023-10-23 13:23:09 -0500

    feat: login, register and home initial pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tempo_app/pages/login.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Poppins',
+        primaryColor: Colors.blue,
+        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
+      home: const Login()
     );
   }
 }
